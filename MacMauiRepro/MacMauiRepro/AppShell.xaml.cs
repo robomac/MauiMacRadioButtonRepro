992eb023b72ca91a4c06d3fabad484f76b214c35
Tony McNamara <tony@kittycentral.net>
2023-04-09 12:35:33 -0700

    Repro case.
    The only change from the default template is a single line in MainAppl.xaml:             <RadioButton Content="Test Button"/>
    However, this entire repro repo is required by the bug filing process.

---
+++ MacMauiRepro/MacMauiRepro/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MacMauiRepro;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ MacMauiRepro/MacMauiRepro/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MacMauiRepro;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
